0b22647af3d981ab347662e1e48edcef337097ce
k-s-jutjoy <kos.saito.jutjoy@gmail.com>
2025-10-28 22:04:32 +0900

    確認

--- src/main/java/com/example/demo/controller/HelloSpringBootWebController.java
+++ src/main/java/com/example/demo/controller/HelloSpringBootWebController.java
@@ -1,12 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import ch.qos.logback.core.joran.spi.HttpUtil.RequestMethod;
-import org.springframework.ui.Model; 
 
 @Controller
 public class HelloSpringBootWebController {
@@ -17,7 +15,7 @@ public class HelloSpringBootWebController {
     }
 
     @PostMapping("/result")
-    public String send(@RequestParam("inputvalue")String inputvalue, Model model) {
+    public String send(@RequestParam String inputvalue, Model model) {
         model.addAttribute("message", inputvalue);
         return "result";
     }
